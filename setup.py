8700712a246917e82efe11308037b300c0a9569a
Bavo Van Achte <bvn@melexis.com>
2016-07-03 19:23:15 +0200

    Added files needed for PyPi registration

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sphinx-wavedrom',
+  packages = ['sphinx-wavedrom'],
+  version = '1.0',
+  description = 'A sphinx extension that allows generating wavedrom diagrams based on their textual representation',
+  author = 'Bavo Van Achte',
+  author_email = 'bavo.van.achte@gmail.com',
+  url = 'https://github.com/bavovanachte/sphinx-wavedrom',
+  download_url = 'https://github.com/bavovanachte/sphinx-wavedrom/tarball/1.0',
+  keywords = ['sphinx', 'wavedrom', 'documentation'],
+  classifiers = [],
+)
