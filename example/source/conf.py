495f57877d433862572034a0fc9f392bec6c018c
Bavo Van Achte <bvn@melexis.com>
2016-11-21 21:04:40 +0100

    Increase page width in rtd theme

--- example/source/conf.py
+++ example/source/conf.py
@@ -117,6 +117,11 @@ todo_include_todos = False
 #html_theme = 'alabaster'
 html_theme = "sphinx_rtd_theme"
 
+html_context = {
+    'css_files': [
+        '_static/style/custom.css',  # override wide tables in RTD theme
+        ],
+     }
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
