ff5f51d59e18a1eb7704616a856fa3090e293537
Bavo Van Achte <bavo.van.achte@gmail.com>
2020-08-18 17:11:39 +0200

    Better output format support

--- sphinxcontrib/wavedrom_cli_wrapper.py
+++ sphinxcontrib/wavedrom_cli_wrapper.py
@@ -35,14 +35,35 @@ def render_wavedrom_cli(self, node, outpath, bname, format):
     with open(input_json, 'w') as f:
         f.write(node['code'])
     try:
-        try:
-            p = subprocess.run(generate_wavedrom_args(self, input_json, output_svg), capture_output=True)
-        except OSError as err:
-            if err.errno != ENOENT:
-                raise
-            raise PlantUmlError('wavedrom command %r cannot be run' % self.builder.config.wavedrom_cli)
-        if p.returncode != 0:
-            raise Exception('error while running wavedrom\n\n%s' % p.stderr)
-        return output_svg
-    finally:
-        f.close()
+        p = subprocess.run(generate_wavedrom_args(self, input_json, output_svg), capture_output=True)
+    except OSError as err:
+        if err.errno != ENOENT:
+            raise
+        raise PlantUmlError('wavedrom command %r cannot be run' % self.builder.config.wavedrom_cli)
+    if p.returncode != 0:
+        raise Exception('error while running wavedrom\n\n%s' % p.stderr)
+
+        # SVG can be directly written and is supported on all versions
+    if format == 'image/svg+xml':
+        fname = "{}.{}".format(bname, "svg")
+        return fname
+
+    # It gets a bit ugly, if the output does not support svg. We use cairosvg, because it is the easiest
+    # to use (no dependency on installed programs). But it only works for Python 3.
+    try:
+        import cairosvg
+    except:
+        raise SphinxError(__("Cannot import 'cairosvg'. In Python 2 wavedrom figures other than svg are "
+                             "not supported, in Python 3 ensure 'cairosvg' is installed."))
+
+    if format == 'application/pdf':
+        fname = "{}.{}".format(bname, "pdf")
+        fpath = os.path.join(outpath, fname)
+        cairosvg.svg2pdf(url=output_svg, write_to=fpath)
+        return fname
+
+    if format == 'image/png':
+        fname = "{}.{}".format(bname, "png")
+        fpath = os.path.join(outpath, fname)
+        cairosvg.svg2png(url=output_svg, write_to=fpath)
+        return fname
